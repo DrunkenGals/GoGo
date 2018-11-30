b4f2721e7afff987e37fde8a05f381c499ea7f9e
Hannayoung <hny4813@naver.com>
2018-11-30 16:38:02 +0900

    nayoung update2

--- hello-react/src/components/detail/detail.js
+++ hello-react/src/components/detail/detail.js
@@ -1,1 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Box, Icon} from 'gestalt';
+
+/* Gestalt */
+import "gestalt/dist/gestalt.css";
+import "../../App.css";
+// import "../main.css";
+
+ class Detail extends Component {
+  render() {
+    return (
+      <Box alignItems="center" display="flex">
+      <Box marginRight={1} padding={1}>
+      <Icon icon="smiley-outline" color="white" />
+      </Box>
+      <div className="text"> 
+        더 파운틴
+      </div>
+      </Box>
+    )
+  }
+}
+
+export default Detail;
